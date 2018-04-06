93497e1d6fbae89dc6ea88eded36c8d142d39b6d
Anson Horton <anson.horton@gmail.com>
2018-04-06 10:57:09 -0700

    Add project files.

---
+++ PRTrackerUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace PRTrackerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
